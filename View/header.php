e44fa7190bd266a5089d5ff4d577238b4c6ee3c9
Iván García Fernández <ivan.garcia@raminatrans.com>
2016-09-16 10:30:56 +0200

    Puesta a punto del proyecto.
    by: Iván García Fernández

--- View/header.php
+++ View/header.php
@@ -2,25 +2,29 @@
 	<head> 
 		<meta charset = "UTF-8"/>
 	    <title>Pokémon DAW</title>
+	    <!--  
+	    Insertar Favicon
+	    <link rel="shortcut icon" type="image/x-icon" href="app/view/img/favicon.gif"/> 
+		-->
 	    <link href='view/css/header.css' rel='stylesheet' type='text/css'>
 	    <link href='view/css/footer.css' rel='stylesheet' type='text/css'>
 	    <link href='view/css/login.css' rel='stylesheet' type='text/css'>
-	   	<link href='view/css/login.css' rel='stylesheet' type='text/css'>
-	   	<link href='view/css/carousel.css' rel='stylesheet' type='text/css'>
-	   	<link href='view/css/carousel.js' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 	</head>
 
 	<body>
 		<div>
-		  	<img class="logo" src="view/img/pokemonDAW.png" alt="logo">
+		  	<img class="logo" src="view/img/logo.png" alt="logo">
 			<div class="menu">	
 				<nav>
 					<ul>
 						<li><a href="#">Hacks</a></li>
 						<li><a href="#">Financiación</a></li>
 						<li><a href="#">Smartwatch</a></li>
-						<li class="alex">Login<a href="#">  </a></li>
+						<li class="alex"><a href="#">  </a></li>
 					</ul>
 				</nav>
 			</div>
--- View/vistanoticia.php
+++ View/vistanoticia.php
@@ -3,14 +3,13 @@
     echo "<div class='estructuraNoticia'>
         <div class='noticiaIzquierda'>
           <img src='".$imagen."' alt='imagen no disponible' class='imgNoticia'/>
-          <div class='subNoticiaIzquierda'>
-            <h2 class='tituloNoticia'>".$titulo."</h2>
-            <p class='fechaNoticia'>".$fecha."</p>
-          </div>
+          
         </div>
 
         <div class='noticiaDerecha'>
           <h3 class='categoriaNoticia'>".$categoria."</h3>
+          <h2 class='tituloNoticia'>".$titulo."</h2>
+          <p class='fechaNoticia'>".$fecha."</p>
           <p class='cuerpoNoticia'>".$cuerpo."</p>
           <div class='valoracion'>";
           for($i=0;$i<5;$i++){
