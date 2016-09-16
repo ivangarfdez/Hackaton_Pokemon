855505adeeb3554557e4a0cda8acc8650cf2af62
pablo <pablomaganapablomagub@gmail.com>
2016-09-16 09:00:39 +0200

    16/09/16-9/00
    sincronización nueva estructura de carpetas

--- View/pages/signin.php
+++ View/pages/signin.php
@@ -2,15 +2,24 @@
 <html>
 <head>
 <title>registro</title>
-<link rel="stylesheet" type="text/css" href="./Css/styleModal.css">
+<link rel="stylesheet" type="text/css" href="../Css/login.css">
 </head>
-
 <body>
 
 	<button id="mostrar">Open Modal</button>
-
 	<div id="modal" class="modal">
+
 		<span class="close">x</span>
+
+		<ul>
+			<li id="btnlogin" class="activo">Login</li>
+			<li id="btnregistrar" class="">registro</li></ul>
+
+		<form action="asdg.php" id="login">
+			<input type="text" placeholder="E-mail"><br/>
+			<input type="text" placeholder="password"><br/>
+			<input type="submit" name="login" value="Entrar">
+		</form>
 
 		<form action="asdg.php" id="registro">
 			<input type="text" placeholder="nombre"><br/>
@@ -20,13 +29,8 @@
 			<input type="text" placeholder="fecha nacimiento"><br/>
 			<input type="submit" name="registrar" value="registrar">
 		</form>
-
-		<form action="asdg.php" id="login">
-			<input type="text" placeholder="E-mail"><br/>
-			<input type="text" placeholder="password"><br/>
-			<input type="submit" name="login" value="Entrar">
-		</form>
+		
 	</div>
-	<script src="./Js/modal.js"></script>
+	<script src="../Js/modal.js"></script>
 </body>
 </html>
