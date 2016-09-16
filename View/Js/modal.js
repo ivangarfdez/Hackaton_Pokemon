f3b5e965c3a41a05e938ca3b7bd14e9ca8e37182
pablo <pablomaganapablomagub@gmail.com>
2016-09-16 08:40:14 +0200

    formulario login y registro
    formulario login y registro javascript sin integrar

--- View/Js/modal.js
+++ View/Js/modal.js
@@ -2,10 +2,32 @@ var modal = document.getElementById('modal')
 var btn = document.getElementById('mostrar');
 var span = document.getElementsByClassName("close")[0];
 
+var btnlogin=document.getElementById('btnlogin');
+var btnregistrar=document.getElementById('btnregistrar');
+
+var formlogin= document.getElementById('login');
+var formregistro= document.getElementById('registro');
+
 btn.onclick = function(){
 	modal.style.display="block";
 }
- 
+
 span.onclick = function() {
     modal.style.display = "none";
 }
+
+
+btnlogin.onclick= function(){
+	btnlogin.className="activo";
+	btnregistrar.className="";
+	formlogin.style.display="block";
+	formregistro.style.display="none";
+}
+btnregistrar.onclick= function(){
+	btnlogin.className="";
+	btnregistrar.className="activo";
+	formlogin.style.display="none";
+	formregistro.style.display="block";
+}
+
+formregistro.style.display="none";
