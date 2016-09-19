590c71f04173e7445d09d0be0034e35f0322249f
pablo <pablomaganapablomagub@gmail.com>
2016-09-19 11:38:08 +0200

    calma
    calma

--- index.php
+++ index.php
@@ -9,17 +9,11 @@
 //cargar 5 ultimsa noticias
 
 $noticias=obtenerCincoUltimasNoticias();
-<<<<<<< HEAD
 
 foreach ($noticias as $noticia) {
 
 modeloNoticia($noticia["titulo"],$noticia["imagen"],$noticia["fecha"],$noticia["nombre"],$noticia["descripcion"],$noticia["valoracion"]);
 }
-=======
-//foreach ($noticia => $noticias) {
-//	modeloNoticia($noticia.titulo,$noticia.imagen,$noticia.fecha,$noticia.categoria,$noticia.descripcion,$noticia.valoracion);
-//}
->>>>>>> origin/master
 	require('view/pages/footer.php');
 	}else {
 		require('view/pages/inicio.php');
