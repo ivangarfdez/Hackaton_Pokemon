54886ae284f830b4530e5de31d5c5a557de640ab
pablo <pablomaganapablomagub@gmail.com>
2016-09-20 01:25:37 +0200

    carousel

--- View/pages/carousel.php
+++ View/pages/carousel.php
@@ -3,23 +3,25 @@
         <input type="radio" name="slider-select-element" id="element1" checked="checked" />
         <input type="radio" name="slider-select-element" id="element2" />
         <input type="radio" name="slider-select-element" id="element3" />
+        <input type="radio" name="slider-select-element" id="element4" />
+        <input type="radio" name="slider-select-element" id="element5" />
 
         <div id="slider-container">
             <div id="slider-box">
                 <div class="slider-element">
-                    <article class="element-red">
-
-                    </article>
+                    <img height="300px" src="./view/img/1.png" alt="noticia 1"/>
                 </div>
                 <div class="slider-element">
-                    <article class="element-green">
-
-                    </article>
+                    <img height="300px" src="./view/img/2.png" alt="noticia 2"/>
                 </div>
                 <div class="slider-element">
-                    <article class="element-blue">
-
-                    </article>
+                    <img height="300px" src="./view/img/3.png" alt="noticia 3"/>
+                </div>
+                <div class="slider-element">
+                    <img height="300px" src="./view/img/4.png" alt="noticia 4"/>
+                </div>
+                <div class="slider-element">
+                    <img height="300px" src="./view/img/5.png" alt="noticia 5"/>
                 </div>
             </div>
         </div>
@@ -28,6 +30,8 @@
             <label for="element1"></label>
             <label for="element2"></label>
             <label for="element3"></label>
+            <label for="element4"></label>
+            <label for="element5"></label>
         </div>
     </section>
 </div>
